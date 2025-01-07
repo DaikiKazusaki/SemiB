730d5c7cb1a4a85067a12de11f1c9b1a31914379
09B22026 <09B22026@localhost>
2025-01-07 15:14:54 +0900

    self-playを実装

--- Main/main.py
+++ Main/main.py
@@ -7,17 +7,11 @@ import gymnasium as gym
 # カスタム環境を登録するか、直接インスタンス化
 env = Environment()
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=100000)  # 任意
+model1 = PPO("MlpPolicy", env, verbose=1)
+model2 = PPO("MlpPolicy", env, verbose=1)
 
-# 学習後にテスト
-obs, info = env.reset()
-done = False
-tmp=obs.copy()
-move_list=[] #ここに履歴を保存
-while not done:
-    action, _ = model.predict(obs)
-    obs, reward, done, truncated, info = env.step(action)
+
+def calcurate_differences(tmp, obs, move_list):
     changed_indices=np.where(tmp != obs) #1step前との差分を取得
     change_list=[]
     for idx in zip(*changed_indices):#差分すべてをチェック
@@ -29,6 +23,40 @@ while not done:
             change_list.append([x, y]) 
         tmp[idx]=value #ひとつ前の盤面を更新
     move_list+=change_list #move_listに結合
-print("Final reward:", reward)
 
+def self_play(env, model1, model2, total_matches = 100):
+    
+    #自己対戦
+    for match in range(total_matches):
+        obs, info = env.reset()
+        done = False
+        current_player = 1
+        tmp=obs.copy()
+        move_list=[] #ここに履歴を保存
+        
+        while not done:
+            model = model1 if current_player == 1 else model2
+            action, _ = model.predict(obs)
+            obs, reward, done, truncated, info = env.step(action)
+            calcurate_differences(tmp, obs, move_list)
+            
+            if done:
+                break
+
+            current_player *= -1
+
+self_play(env, model1, model2, total_matches = 10)
+
+# 学習後にテスト
+obs, info = env.reset()
+done = False
+tmp=obs.copy()
+move_list=[] #ここに履歴を保存
+while not done:
+    action, _ = model1.predict(obs)
+    obs, reward, done, truncated, info = env.step(action)
+    calcurate_differences(tmp, obs, move_list)
+
+print("Final reward:", reward)
 renderer.render(move_list, interval=2000)
+
